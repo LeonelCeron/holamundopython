6324a0492e4445e565b2e034a3b80e85c62a6794
Leonel Ceron <ceronleonel99@gmail.com>
2023-10-06 08:09:06 -0600

    Cuarto Commit - Prueba

--- main.py
+++ main.py
@@ -13,3 +13,6 @@ async def ruta_de_prueba():
     return {"Message": "Hola grupo 08 desde Fast_API v1"}
 
 
+@app.post("/insertar")
+async def insertar_prueba(nombre: Nombre):
+    return {"Message": "Los datos del usuario son los siguientes","nombre":{nombre.nombre},"apellido":{nombre.apellido},"edad":{nombre.edad}}
