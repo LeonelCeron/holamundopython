82f7c4507850a09272234c0dd4dffadcf78ca996
Leonel Ceron <ceronleonel99@gmail.com>
2023-10-05 23:55:22 -0600

    Tercer Commit - Prueba

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Nombre(BaseModel):
+    nombre: str
+    apellido: str
+    edad: int
 
 @app.get("/inicio")
 async def ruta_de_prueba():
     return {"Message": "Hola grupo 08 desde Fast_API v1"}
+
+
+@app.post("/insertar")
+async def insertar_prueba(nombre: Nombre):
+    return {"Message": "Los datos del usuario son los siguientes","nombre":{nombre.nombre},"apellido":{nombre.apellido},"edad":{nombre.edad}}
